edfa923157b5b4a0ac68426b45ed04404ad1a38d
trossenmobileai <trossenmobileai@gmail.com>
2026-06-08 12:49:32 +0200

    Modify script to show MobileAI tasks

--- scripts/tools/list_envs.py
+++ scripts/tools/list_envs.py
@@ -66,7 +66,7 @@ def main():
     index = 0
     # acquire all Isaac environments names
     for task_spec in gym.registry.values():
-        if "WXAI" in task_spec.id:
+        if "WXAI" in task_spec.id or "MobileAI" in task_spec.id:
             # add details to table
             table.add_row(
                 [
